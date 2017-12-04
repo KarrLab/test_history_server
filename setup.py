79b9bae3b3d81ecba3ef7e99e8ceb7b39f604d49
Jonathan Karr <jonrkarr@gmail.com>
2017-12-04 15:47:25 -0500

    updating to pkg_utils

--- setup.py
+++ setup.py
@@ -7,55 +7,43 @@ Installation script
 :License: MIT
 """
 
-from setuptools import setup, find_packages
+import setuptools
+try:
+    import pkg_utils
+except ImportError:
+    import pip
+    pip.main(['install', 'git+https://github.com/KarrLab/pkg_utils.git#egg=pkg_utils'])
+    import pkg_utils
 import os
-import re
 
-# get long description
-if os.path.isfile('README.rst'):
-    with open('README.rst', 'r') as file:
-        long_description = file.read()
-else:
-    long_description = ''
+name = 'test_history_server'
+dirname = os.path.dirname(__file__)
 
-# get version
-with open('test_history_server/VERSION', 'r') as file:
-    version = file.read().strip()
-
-# parse dependencies and their links from requirements.txt files
-install_requires = []
-dependency_links = []
-
-for line in open('requirements.txt'):
-    pkg_src = line.rstrip()
-    match = re.match('^.+#egg=(.*?)$', pkg_src)
-    if match:
-        pkg_id = match.group(1)
-        dependency_links.append(pkg_src)
-    else:
-        pkg_id = pkg_src
-    install_requires.append(pkg_id)
+# get package metadata
+md = pkg_utils.get_package_metadata(dirname, name)
 
 # install package
-setup(
-    name="test_history_server",
-    version=version,
+setuptools.setup(
+    name=name,
+    version=md.version,
     description="Unit test history server",
-    long_description=long_description,
+    long_description=md.long_description,
     url="http://tests.karrlab.org",
-    download_url='https://github.com/KarrLab/test_history_server',
+    download_url='https://github.com/KarrLab/' + name,
     author="Jonathan Karr",
     author_email="jonrkarr@gmail.com",
     license="MIT",
     keywords='xunit junit unit test history',
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
     package_data={
         'test_history_server': [
             'VERSION',
         ],
     },
-    install_requires=install_requires,
-    dependency_links=dependency_links,
+    install_requires=md.install_requires,
+    extras_require=md.extras_require,
+    tests_require=md.tests_require,
+    dependency_links=md.dependency_links,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
